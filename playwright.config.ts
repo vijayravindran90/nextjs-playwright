9ed467b47f4b071e5f4578e7dc726698503993fd
vijayravindran90 <108650169+vijayravindran90@users.noreply.github.com>
2026-01-12 06:58:50 +0000

    Set baseURL for Playwright tests to the deployed site

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,6 +30,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    baseURL:'https://vijayravindran90.github.io/nextjs-playwright/',
     headless: true,
   },
 
