9dbb1985465eead8219240646cfe302f91a2e939
vijayravindran90 <108650169+vijayravindran90@users.noreply.github.com>
2026-01-12 06:53:51 +0000

    Update Node.js version in deployment workflow and change basePath in Next.js config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/codespaces-blank',
+  basePath: '/nextjs-playwright',
   images: {
     unoptimized: true,
   },
