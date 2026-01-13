7f52dbba5d3f9ac33b4d0235c314c2c587e97132
vijayravindran90 <108650169+vijayravindran90@users.noreply.github.com>
2026-01-13 09:29:18 +0000

    Update the api test endpoint

--- tests/api.spec.ts
+++ tests/api.spec.ts
@@ -3,7 +3,7 @@ import { test,expect } from '@playwright/test';
 test('verify and validate the API testing page @api @smoke', async ({page,request}) => {
   await page.goto(`/`);
   const [response] = await Promise.all([
-    page.waitForResponse(resp => resp.url().includes('**/shop/v2') && resp.status() === 200),
+    page.waitForResponse(resp => resp.url().includes('https://api.predic8.de/shop/v2/products/8') && resp.status() === 200),
     page.locator('a[href*="/api"]').first().click(),
   ]);
   const responseBody = await response.json();
