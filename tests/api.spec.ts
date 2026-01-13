fb6b53542576b174d39ad13956aec3b9d36127fc
vijayravindran90 <108650169+vijayravindran90@users.noreply.github.com>
2026-01-13 09:24:53 +0000

    Add api based test

--- tests/api.spec.ts
+++ tests/api.spec.ts
@@ -3,7 +3,10 @@ import { test,expect } from '@playwright/test';
 test('verify and validate the API testing page @api @smoke', async ({page,request}) => {
   await page.goto(`/`);
   const [response] = await Promise.all([
-    page.waitForResponse(resp => resp.url().includes('/api/data') && resp.status() === 200),
+    page.waitForResponse(resp => resp.url().includes('**/shop/v2') && resp.status() === 200),
     page.locator('a[href*="/api"]').first().click(),
   ]);
+  const responseBody = await response.json();
+    expect(responseBody.name).toHaveText('Grapes');
+    expect(responseBody.price).toEqual('4.5');
 });
